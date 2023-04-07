f1528f19b9167c87c10ff5abdaaf9a4da7e8cb53
Silvio Colman <silviorafael.colman@gmail.com>
2023-04-07 14:43:46 -0400

    Initial Commit

---
+++ TaskPlanner/TaskPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskPlannerApp.swift
+//  TaskPlanner
+//
+//  Created by Silvio Colmán on 2023-04-07.
+//
+
+import SwiftUI
+
+@main
+struct TaskPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
